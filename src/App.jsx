b75bb972182ef991e9762a0476858d5b407442e0
minarhossain <codewithmnr@gmail.com>
2023-09-19 00:02:01 +0600

    this is minar hossain menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 function App() {
-  return <h1>The Restaurant App</h1>;
+  return (
+    <>
+      <h1>The Restaurant App</h1>;
+      <h2>Mnar hossain menu</h2>
+    </>
+  )
 }
 
 export default App;
