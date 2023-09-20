56f87c6aabea3bea31ff0cab402da8ab2f1a54d2
Mredul Kanthy Dhar <mreduldhar237@gmail.com>
2023-09-20 20:30:18 +0600

    created navbar and footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
-function App() {
-  return <h1>The Restaurant App</h1>;
-}
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import NotFound from './pages/NotFound';
+import AppNavbar from './components/Navbar/AppNavbar';
+import Footer from './components/footer/Footer';
+
+
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <AppNavbar/>
+        
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+          
+          <Route path='*' element={<NotFound/>} />
+        </Routes>
+        
+        <Footer/>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
