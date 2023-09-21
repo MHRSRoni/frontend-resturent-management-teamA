9cf58f290eac8750ad55e064a57ff83e99907111
minarhossain <codewithmnr@gmail.com>
2023-09-21 22:03:33 +0600

    details section updaed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Products from "./components/Products/Products";
 import Search from "./components/SearchBox/Search";
-
+import Main from "./layouts/Main";
+import DetailsProduct from "./components/DetailsProduct/DetailsProduct";
 function App() {
-  return (
-    <>
-      <Search />
-      <Products />
 
-    </>
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Products></Products>
+        },
+        {
+          path: 'details/:detailsId',
+          loader: async ({ params }) => {
+            console.log(params.detailsId);
+            return fetch(`https://backend-resturent-team-a.onrender.com/api/v1/food/${params.detailsId}`)
+          },
+          element: <DetailsProduct></DetailsProduct>
+        }
+      ]
+    }
+
+  ])
+
+
+  return (
+
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
   )
 }
 
