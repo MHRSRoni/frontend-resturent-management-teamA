6932ec278864f35e51635c18ea1cb41d79be79b8
Hasib <hasibhosen7612@gmail.com>
2023-09-21 20:09:21 +0600

    Add/About us added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,6 +10,7 @@ import "swiper/css/navigation";
 import sliderImage1 from "../assets/images/slider-1.jpg";
 import sliderImage2 from "../assets/images/slider-2.jpg";
 import sliderImage3 from "../assets/images/slider-3.jpg";
+import aboutUs from "../assets/images/about-img.jpg";
 
 import "./HomePage.css";
 
@@ -59,7 +60,21 @@ const HomePage = () => {
       {/* Deal- Section */}
 
       {/* About- Section */}
-
+      <div className="about-section">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <img src={aboutUs} alt="" />
+            </div>
+            <div className="col">
+              <h2>About Us</h2>
+              <p>Proident qui et qui aliquip aliquip anim consectetur pariatur et deserunt anim ipsum qui culpa. Lorem cillum labore nisi consectetur velit ea tempor amet nulla labore cupidatat proident. Proident laboris aliquip laboris nisi in. Duis minim magna nostrud minim fugiat duis eu ad consequat ad. Eiusmod elit non velit laborum aliqua nostrud qui duis nulla ea consequat consectetur elit culpa. Officia exercitation id minim eu nisi in.</p>
+              <br />
+              <p>Non ex enim dolor ipsum duis fugiat. Elit mollit reprehenderit occaecat voluptate cupidatat enim enim fugiat ipsum quis labore elit nulla. Qui culpa cupidatat velit in incididunt cillum laboris. Elit mollit dolor magna anim dolore mollit sunt dolor sunt elit. Esse qui excepteur nostrud aliqua voluptate ad ea dolor ipsum fugiat cillum eu eu aute. Cillum esse est mollit aute duis sit excepteur id quis qui eiusmod labore dolor. Ullamco laboris mollit nostrud consectetur fugiat proident dolor elit magna.</p>
+            </div>
+          </div>
+        </div>
+      </div>
       {/* Chefs- Section */}
       <Chef />
 
