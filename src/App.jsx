5c5c7317ac46181982bf515ae43d0c170c688ffb
Mredul kanthy Dhar <72749608+mreduldhar@users.noreply.github.com>
2023-09-21 22:24:29 +0600

    Mredul/navbar footer (#10)
    * created navbar and footer section
    
    * created chef section
    
    * merge conflict reslove
    
    ---------
    
    Co-authored-by: Ali Rafat <coderrafat@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import NotFound from './pages/NotFound';
 import AppNavbar from './components/Navbar/AppNavbar';
+import Footer from './components/footer/Footer';
 
 
 import Products from "./components/Products/Products";
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
 import './App.css'
-=======
-{
-  /* The following line can be included in your src/index.js or App.js file */
-}
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
   //<React.StrictMode>
   <App />
   //</React.StrictMode>,
 )
+
