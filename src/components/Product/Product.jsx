fb4a9f97f5ed0c1bd3645e24212fe861ccf06311
Minar Hossain <95146879+minarhossain@users.noreply.github.com>
2023-09-21 22:20:43 +0600

    Minar/ourmenu (#11)
    * this is minar hossain menu
    
    * our section design is ready
    
    * container updated
    
    * details section updaed
    
    ---------
    
    Co-authored-by: Ali Rafat <coderrafat@gmail.com>

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,13 @@
+import { useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 
 const Product = ({ food }) => {
-    console.log(food);
-    const { title, index, picture, name, price, calories } = food;
+    // console.log(food);
+    const { _id, title, index, picture, name, price, calaory } = food;
+
+
     return (
 
 
@@ -17,13 +21,17 @@ const Product = ({ food }) => {
                 </Card.Text>
                 <div className='d-flex justify-content-between align-items-center mb-3'>
                     <h6>Price {price}</h6>
-                    <h6>Calories: {calories} </h6>
+                    <h6>Calories: {calaory} </h6>
                 </div>
                 <div>
 
 
                     <Button variant="outline-warning text-dark">+ Add to Cart</Button>
-                    <Button variant="outline-warning text-dark ms-5">Details </Button>
+
+                    <Link to={`/details/${_id}`}>
+                        <Button variant="outline-warning text-dark ms-5">Details </Button>
+                    </Link>
+
                 </div>
             </Card.Body>
         </Card>
